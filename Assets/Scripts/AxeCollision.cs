8fce24c1a949bd1f090850486e207e1e29eca569
unknown <harounali2018@gmail.com>
2019-07-07 19:13:15 -0700

    Added movement and axe interaction

---
+++ Assets/Scripts/AxeCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeCollision : MonoBehaviour
+{
+    public GameObject woodParticlePrefab;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "TriggerCut")
+        {
+            Debug.Log("hitting tree");
+
+            // spawn particles at collision's position
+            Vector2 linePos = collision.transform.position;
+
+            GameObject woodParticleObject = Instantiate(woodParticlePrefab);
+            woodParticleObject.transform.position = transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/ViveInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 touchPadAction;
+    public Camera playerCamera;
+    public float playerSpeed;
+    public Rigidbody rb;
+
+    void Update()
+    {
+        Vector2 touchpadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        // will move only foward in the camera's general direction for now with no vertical movement
+        if(touchpadValue != Vector2.zero)
+        {
+            Vector3 cameraDirection = playerCamera.transform.forward;
+            Vector3 moveDirection = new Vector3(cameraDirection.x, 0, cameraDirection.z);
+
+            rb.MovePosition(moveDirection * playerSpeed * Time.deltaTime + transform.position);
+            //transform.position += moveDirection * playerSpeed * Time.deltaTime;
+        }
+    }
+}
